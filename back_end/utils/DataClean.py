0092486832579110b47a482fbc53363d995be2bc
JoyTrndsttr <7742947+JoyTrndsttr@user.noreply.gitee.com>
2024-09-18 13:25:02 +0800

    Update DataClean.py

--- back_end/utils/DataClean.py
+++ back_end/utils/DataClean.py
@@ -1,4 +1,3 @@
-
 def check_overall_std(data):
     overall_std = data['数据'].std() # 计算整体方差
     threshold = 0.001  # 方差的阈值
@@ -15,7 +14,7 @@ def check_waveform(type, data):
     if type in ["LDQJ"]: #梁端倾角 小抖动+大抖动+小抖动
         return True
     if type in ["ZDJSD2", "ZDJSD1", "SXJSDX", "SXJSDZ", "SXJSDY"]: #梁体横向/竖向加速度，轨道板横向/竖向/纵向加速度
-        return True 
+        return True
     if type in ["XDWY1", "XDWY2"]:#轨道板相对横向位移/相对竖向位移
         return True
     if type in ["ZDWY2", "ZDWY1"]: #梁体横向/纵向振幅 0附近抖动+正弦波+大抖动+正弦波+0附近抖动
